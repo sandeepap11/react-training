96164a7d08183b126607ed99d522cbdeaf1e1ae5
sandeepramachandra <sandeepap11@gmail.com>
2020-07-22 15:36:17 +0530

    focus events

--- src/App.js
+++ src/App.js
@@ -16,11 +16,17 @@ class App extends React.Component {
           type="text"
           style={{ height: "40px", width: "200px", margin: "5% 20%" }}
           ref={this.inputRef}
+          onFocus={() => {
+            this.inputRef.current.style.backgroundColor = "white";
+            this.inputRef.current.style.color = "black";
+          }}
         />
         <button
-          onClick={() =>
-            this.setState({ inputValue: this.inputRef.current.value })
-          }
+          onClick={() => {
+            this.setState({ inputValue: this.inputRef.current.value });
+            this.inputRef.current.style.backgroundColor = "rebeccapurple";
+            this.inputRef.current.style.color = "white";
+          }}
         >
           Show Value
         </button>
