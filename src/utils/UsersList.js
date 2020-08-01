00c939f3b73468953e4419f501fd1835f64b236b
sandeepramachandra <sandeepap11@gmail.com>
2020-08-01 19:04:52 +0530

    usereducer

---
+++ src/utils/UsersList.js
@@ -0,0 +1,32 @@
+export const USERS = [
+  {
+    id: 1,
+    name: "Allen Saint-Maximin",
+    email: "allen.saint-maximin@robo.com",
+    isActive: true
+  },
+  {
+    id: 2,
+    name: "Rosie McCann",
+    email: "rosie.mccann@robo.com",
+    isActive: true
+  },
+  {
+    id: 3,
+    name: "Theodore S Horne",
+    email: "theo.horne@robo.com",
+    isActive: true
+  },
+  {
+    id: 4,
+    name: "Beatriz Barros",
+    email: "beatriz.barros@robo.com",
+    isActive: true
+  },
+  {
+    id: 5,
+    name: "Lisa Radcliffe",
+    email: "lisa.r@robo.com",
+    isActive: true
+  }
+];
